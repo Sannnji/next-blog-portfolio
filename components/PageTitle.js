aea48a6029f98793f19cd8f1dde8e39636fd69bd
Sannnji <jamesji.penfield@protonmail.com>
2021-09-21 13:11:16 -0400

    added social media icon buttons

--- components/PageTitle.js
+++ components/PageTitle.js
@@ -1,4 +1,5 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Text, ButtonGroup, IconButton } from "@chakra-ui/react";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 export const PageTitle = ({ title, subtitle }) => {
   return (
@@ -9,6 +10,22 @@ export const PageTitle = ({ title, subtitle }) => {
       <Text fontSize="4xl" fontWeight="thin">
         {subtitle}
       </Text>
+      <ButtonGroup variant="solid" color="gray.600">
+        <IconButton
+          as="a"
+          href="https://www.linkedin.com/in/jamesji98/"
+          aria-label="LinkedIn"
+          color="white"
+          icon={<FaLinkedin fontSize="20px" />}
+        />
+        <IconButton
+          as="a"
+          href="https://github.com/Sannnji"
+          aria-label="GitHub"
+          color="white"
+          icon={<FaGithub fontSize="20px" />}
+        />
+      </ButtonGroup>
     </Box>
   );
 };
