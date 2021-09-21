728da8ff708dd261f78cc2862a5a5fbea411c6dc
Sannnji <jamesji.penfield@protonmail.com>
2021-09-21 16:01:42 -0400

    static margin

--- components/Navbar/NavLink.js
+++ components/Navbar/NavLink.js
@@ -2,7 +2,7 @@ import { Box } from "@chakra-ui/react";
 
 export const NavLink = ({ children }) => {
   return (
-    <Box mx={{ base: 0, md: 2 }} my={{ base: 2, md: 0 }}>
+    <Box mx={2}>
       {children}
     </Box>
   );
