cb18330f7591d91b5177955078aeeee00fcfba95
Sannnji <jamesji.penfield@protonmail.com>
2021-09-27 17:15:19 -0400

    added new components for markdown

--- components/MDXComponents.js
+++ components/MDXComponents.js
@@ -6,14 +6,34 @@ import {
   Link,
   Text,
   Divider,
-  useColorMode,
+  useColorModeValue,
 } from "@chakra-ui/react";
 
+const CodeBlock = (props) => {
+  return (
+    <Box
+      bg={useColorModeValue("gray.100", "gray.600")}
+      borderRadius="lg"
+      p={4}
+      {...props}
+    />
+  );
+};
+
 const MDXComponents = {
-  h1: (props) => <Heading as="h1" size="xl" my={4} {...props} />,
+  h1: (props) => (
+    <Heading as="h1" size="xl" my={4} fontWeight="semibold" {...props} />
+  ),
+  h2: (props) => (
+    <Heading as="h2" size="lg" my={4} fontWeight="semibold" {...props} />
+  ),
+  h4: (props) => (
+    <Heading as="h4" size="md" my={4} fontWeight="semibold" {...props} />
+  ),
+  p: (props) => <Text my={2} {...props} />,
   ul: (props) => <Box as="ul" pt={2} pl={4} ml={2} {...props} />,
   strong: (props) => <span style={{ color: "#4FD1C5" }} {...props} />,
-  code: (props) => <Box bg="gray.600" border="1px" borderRadius="lg" p={4} {...props}/>
+  code: (props) => <CodeBlock {...props} />,
 };
 
 export default MDXComponents;
