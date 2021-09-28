ad14721901f8c4159082c76889735564429f4a6d
Sannnji <jamesji.penfield@protonmail.com>
2021-09-28 15:13:50 -0400

    added padding Y

--- components/Section.js
+++ components/Section.js
@@ -2,7 +2,7 @@ import { Box, Text } from "@chakra-ui/react";
 
 export const Section = ({ heading, children }) => {
   return (
-    <Box width={{ base: "auto", md: "65%" }}>
+    <Box width={{ base: "auto", md: "75%" }} py={8}>
       <Text fontSize="4xl" my={4}>{heading}</Text>
       <Box>{children}</Box>
     </Box>
