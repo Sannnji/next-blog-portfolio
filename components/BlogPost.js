3c0ff1a8adff60985e34b7097be949a952c28c50
Sannnji <jamesji.penfield@protonmail.com>
2021-11-07 17:30:13 -0500

    removed image

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -4,7 +4,7 @@ import { Box, Flex, Text, Image, useColorModeValue } from "@chakra-ui/react";
 
 export const BlogPost = ({ post }) => {
   const boxShadow = useColorModeValue("lg", "dark-lg");
-  
+
   return (
     <Flex
       flexDir={{ base: "column", md: "column", lg: "row" }}
@@ -13,7 +13,7 @@ export const BlogPost = ({ post }) => {
       w="100%"
       my={16}
     >
-      <Box mr={4} mb={{ base: 4, lg: 0 }} w="75%">
+      <Box mr={4} mb={{ base: 4, lg: 0 }}>
         <Link href={`blog/${post.slug}`} passHref>
           <Text as="a" fontSize="2xl" _hover={{ color: "teal" }}>
             {post.frontmatter.title}
@@ -23,15 +23,6 @@ export const BlogPost = ({ post }) => {
         <Text mb={4}>{post.frontmatter.date}</Text>
         <Text>{post.preview}</Text>
       </Box>
-
-      <Image
-        src={post.frontmatter.image}
-        alt=""
-        w="75%"
-        height="fit-content"
-        objectFit="contain"
-        boxShadow={boxShadow}
-      />
     </Flex>
   );
 };
