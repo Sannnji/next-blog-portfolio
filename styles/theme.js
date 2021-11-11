51999e2c69950fffb2be7dbf76f3088327f6cd81
Sannnji <jamesji.penfield@protonmail.com>
2021-11-11 18:22:25 -0500

    removed focus on buttons globally

--- styles/theme.js
+++ styles/theme.js
@@ -3,6 +3,11 @@ import { extendTheme } from "@chakra-ui/react";
 const theme = extendTheme({
   useSystemColorMode: false,
   components: {
+    Button: {
+      baseStyle: {
+        _focus: { outline: "none", boxShadow: "none" },
+      },
+    },
   },
 });
 
