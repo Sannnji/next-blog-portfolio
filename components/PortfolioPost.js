b2a99a82aea3921e2497b8cc10257f141caa800d
Sannnji <jamesji.penfield@protonmail.com>
2022-05-19 13:45:15 -0400

    restyled into a card

--- components/PortfolioPost.js
+++ components/PortfolioPost.js
@@ -1,48 +1,85 @@
 import Link from "next/link";
 import {
   Flex,
-  Box,
+  Stack,
+  Heading,
   Image,
   Text,
   IconButton,
   Divider,
-  useColorModeValue,
 } from "@chakra-ui/react";
 
 import { FaExternalLinkAlt, FaGithub } from "react-icons/fa";
 
 export const PortfolioPost = ({ post }) => {
-  const boxShadow = useColorModeValue("lg", "dark-lg");
-
   return (
-    <Flex
-      flexDir={{ base: "column", lg: "row" }}
-      alignItems={{ base: "flex-start", lg: "center" }}
-      justifyContent="space-between"
-      w="100%"
-      mb={16}
+    <Stack
+      borderRadius="lg"
+      w={{ sm: "100%", md: "auto" }}
+      direction={{ base: "column", md: "row" }}
+      bg="rgba(255,255,255, 0.2)"
+      boxShadow={"2xl"}
+      padding={4}
+      mt={20}
     >
-      <Box mr={4} w="75%">
+      <Flex flex="1" _hover={{ cursor: "pointer" }}>
         <Link href={`portfolio/${post.slug}`} passHref>
-          <Text as="a" fontSize="2xl" _hover={{ color: "teal" }}>
-            {post.frontmatter.name}
-          </Text>
+          <Image
+            objectFit="cover"
+            align={"0px"}
+            boxSize="100%"
+            src={post.frontmatter.image}
+            alt=""
+          />
         </Link>
-        <Text>{post.frontmatter.date}</Text>
+      </Flex>
+
+      <Stack
+        flex={1}
+        flexDirection="column"
+        justifyContent="center"
+        p={4}
+        pt={2}
+      >
+        {
+          // Title
+        }
+        <Link href={`portfolio/${post.slug}`} passHref>
+          <Heading
+            as="a"
+            fontSize={{ base: "20px", lg: "32px" }}
+            _hover={{ color: "teal" }}
+          >
+            {post.frontmatter.name}
+          </Heading>
+        </Link>
+        {
+          // Date
+        }
+        <Text color="#9B9B9B">{post.frontmatter.date}</Text>
+        {
+          // FrameWorks
+        }
         <Flex mb={4}>
-          <Text>
+          <Text color="#9B9B9B">
             {post.frontmatter.frameworks.map((element, index) => {
               if (index === post.frontmatter.frameworks.length - 1) {
-                return  element ;
+                return element;
               } else {
-                return  element  + " | ";
+                return element + " | ";
               }
             })}
           </Text>
         </Flex>
 
+        {
+          // Post Preview
+        }
         <Text>{post.preview}</Text>
 
+        {
+          // Buttons
+        }
         <Flex
           my={{ base: 4, lg: 0 }}
           mt={{ base: 4, lg: 4 }}
@@ -60,16 +97,7 @@ export const PortfolioPost = ({ post }) => {
             icon={<FaExternalLinkAlt />}
           />
         </Flex>
-      </Box>
-
-      <Image
-        src={post.frontmatter.image}
-        alt=""
-        w="75%"
-        height="fit-content"
-        objectFit="contain"
-        boxShadow={boxShadow}
-      />
-    </Flex>
+      </Stack>
+    </Stack>
   );
 };
