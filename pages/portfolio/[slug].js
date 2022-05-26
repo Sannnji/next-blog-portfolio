062dfb8d84dff1e244865f32c24c2632dc3fce1d
Sannnji <jamesji.penfield@protonmail.com>
2022-05-26 17:31:06 -0400

    created title box component and added external links

--- pages/portfolio/[slug].js
+++ pages/portfolio/[slug].js
@@ -4,8 +4,11 @@ import {
   Text,
   Heading,
   Image,
+  IconButton,
+  Divider,
   useColorModeValue,
 } from "@chakra-ui/react";
+import { FaExternalLinkAlt, FaGithub } from "react-icons/fa";
 import { MDXRemote } from "next-mdx-remote";
 
 import { Layout } from "../../components/Layout";
@@ -14,11 +17,39 @@ import { getFiles, getFrontmatterBySlug } from "../../lib/mdx";
 
 export default function PostPage({
   post: {
-    frontmatter: { name, date, image },
+    frontmatter: { name, date, image, github, link },
     mdxSource,
   },
 }) {
   const boxShadow = useColorModeValue("lg", "2xl");
+
+  const TitleBox = () => {
+    return (
+      <Box
+        zIndex={0}
+        py={4}
+        position="relative"
+        bottom={{ base: 10, md: 24, lg: 20 }}
+        bg="rgba(0, 0, 0, 0.5)"
+        color="white"
+        textAlign="center"
+        borderRadius="lg"
+      >
+        <Heading>{name}</Heading>
+        <Text fontWeight="thin">{date}</Text>
+        <Flex
+          my={{ base: 4, lg: 0 }}
+          mt={{ base: 4, lg: 4 }}
+          justifyContent="center"
+          alignItems="center"
+        >
+          <IconButton as="a" href={github} icon={<FaGithub />} />
+          <Divider mx={4} orientation="vertical" height="25px" bg="white" />
+          <IconButton as="a" href={link} icon={<FaExternalLinkAlt />} />
+        </Flex>
+      </Box>
+    );
+  };
 
   return (
     <Layout>
@@ -46,20 +77,7 @@ export default function PostPage({
             py={{ base: 4, md: 8, lg: 8 }}
             borderRadius="lg"
           >
-            <Box
-              zIndex={0}
-              py={4}
-              position="relative"
-              bottom={{ base: 10, md: 24, lg: 20 }}
-              bg="rgba(0, 0, 0, 0.5)"
-              color="white"
-              textAlign="center"
-              borderRadius="lg"
-            >
-              <Heading>{name}</Heading>
-              <Text fontWeight="thin">{date}</Text>
-            </Box>
-
+            <TitleBox />
             <MDXRemote {...mdxSource} components={MDXComponents} />
           </Box>
         </Flex>
